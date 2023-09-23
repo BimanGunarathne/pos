ca75a269d7fb601d246a14305e47dec06d3e86c3
Biman Gunarathne <bimangunarathne@outlook.com>
2023-09-23 09:48:02 +0530

    4

--- js/main.js
+++ js/main.js
@@ -7,12 +7,21 @@ class User{
 }
 
 let userArr=[];
-
 /*=======User Management*/
 const createUser=()=>{
-    let createdUser=new User('','','');
-    userArr.forEach()
+
+    const fullName=$('#fullName').val();
+    const email=$('#register-email').val();
+    const password=$('#register-password').val();
+
+    let createdUser=new User(fullName,email,password);
+    let existsUserData=userArr.find(e=>e.email===email);
+    if(existsUserData){
+        alert('User email already exists!');
+        return;
+    }
     userArr.push(createdUser);
+    console.log(userArr);
 }
 //
 const clearAndLoad=(element)=>{
