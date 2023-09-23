5166bd4e5435b291e1acde342c605b5fab37aed8
Biman Gunarathne <bimangunarathne@outlook.com>
2023-09-23 08:55:52 +0530

    3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+class User{
+    constructor(fullName,email,password) {
+        this.fullName=fullName;
+        this.email=email;
+        this.password=password;
+    }
+}
+
+let userArr=[];
+
+/*=======User Management*/
+const createUser=()=>{
+    let createdUser=new User('','','');
+    userArr.forEach()
+    userArr.push(createdUser);
+}
+//
 const clearAndLoad=(element)=>{
-    let body = $('body').empty();
-    let tempElement=$('#'+element);
-    tempElement.appendTo(body);
+
 }
